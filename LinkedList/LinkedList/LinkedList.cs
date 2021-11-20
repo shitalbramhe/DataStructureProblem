ef34f070524466be70b4a52dab68bce21c6797cc
shital bramhe <shitalbramhe@gmail.com>
2021-11-20 14:20:28 +0530

    [shital] ADD:data added in reverse order linked list

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Datastructure
+﻿namespace Datastructure
 {
     internal class LinkedList
     {
@@ -26,6 +20,21 @@ namespace Datastructure
                 temp.next = node;
             }
             Console.Write("\n {0} inserted into linked list", node.data);
+        }
+        public void AddinreverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+            Console.Write("\n {0} inserted into linked list", newNode.data);
         }
         internal void Display()
         {
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -11,9 +11,14 @@ namespace Datastructure
         static void Main(string[] args)
         {
             LinkedList linkedlist = new LinkedList();
-            linkedlist.Add(56);
-            linkedlist.Add(30);
-            linkedlist.Add(70);
+            //linkedlist.Add(56);
+            //linkedlist.Add(30);
+            //linkedlist.Add(70);
+            //linkedlist.Display(); 
+
+            linkedlist.AddinreverseOrder(70);
+            linkedlist.AddinreverseOrder(30);
+            linkedlist.AddinreverseOrder(56);
             linkedlist.Display();
             Console.ReadLine();
         }
