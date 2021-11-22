90e8edbc7922473aff76679c0c7310edc735231f
shital bramhe <shitalbramhe@gmail.com>
2021-11-22 14:39:01 +0530

    [shital] ADD:  stack created

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -10,14 +10,19 @@ namespace Datastructure
     {
         static void Main(string[] args)
         {
-            LinkedList linkedlist = new LinkedList();
+            Stack stack = new Stack(); 
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+           /* LinkedList linkedlist = new LinkedList();
             linkedlist.Add(56);
             linkedlist.Add(30);
             linkedlist.Add(40);
             linkedlist.Add(70);
             linkedlist.Display();
             int position = linkedlist.Search(40);
-            linkedlist.DeleteNodeAtParticularPosition(position);
+            linkedlist.DeleteNodeAtParticularPosition(position);*/
         }
     }
 }
